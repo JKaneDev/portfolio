ebbc9aaac3b86cafbf5d79f8375b2d1cef71bb25
James Kane <james.kane59@outlook.com>
2023-05-09 16:21:07 +0100

    preliminary styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='header'>
+				<a href='#' className='logo'>
+					James Kane.
+				</a>
+
+				<nav className='navbar'>
+					<a href='' className='hoverUnderlineEffect'>
+						Home
+					</a>
+					<a href='' className='hoverUnderlineEffect'>
+						About
+					</a>
+					<a href='' className='hoverUnderlineEffect'>
+						Portfolio
+					</a>
+					<a href='' className='hoverUnderlineEffect'>
+						Contact
+					</a>
+				</nav>
+			</header>
+
+			<section className='home'>
+				<div className='home-content'>
+					<h1>Hi, I'm James Kane</h1>
+					<h3>Blockchain Developer</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi consectetur eum beatae quasi molestias eaque
+						asperiores, id quibusdam aliquid blanditiis optio quod placeat tenetur error, nihil, vel accusantium quaerat
+						corporis.
+					</p>
+					<div className='btn-wrapper'>
+						<a href='#'>Hire Me</a>
+						<a href='#'>Reach Out</a>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
